1369057b6728c9b6056ffb2023bea2db6deb0f09
Shivam Negi <72339012+shivamnegi1705@users.noreply.github.com>
2023-03-30 17:53:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Prime Merch Store</h1>
+    </div>
+  );
+}
